aa117c95fee6194502b553a66c067ba5fd492b41
godsenal <tmqps78@gmail.com>
2021-08-31 20:17:22 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/HdiKxrYUJMONYZuBBovrL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/297-795711bc8ea2122b4808.js","static/chunks/pages/index-fe7b8ff8693853693b3e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
