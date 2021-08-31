2d884d31cb065aba0f7e624885f2a658cf29b1dc
Godsenal <tmqps78@gmail.com>
2021-08-31 11:33:34 +0000

    Deploying to gh-pages from @ Godsenal/recoil-study@13df90bc34af59ebe317f1ff93acb1e3cc0d0833 🚀

---
+++ _next/static/z60Re7ESwxcqEFf-njqAM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/160-749c0295e3ec6d5acee7.js","static/chunks/pages/index-63c25ff179c1634ad2fa.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
